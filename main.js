55f78debcd287dd966346d007ec69c2f92cd3861
rizvan008 <108735760+rizvan008@users.noreply.github.com>
2025-02-28 20:43:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,20 +7,6 @@ import {animate} from './assets/animation'
 import { Torus, cube1, cubeAxis } from './assets/models'
 import {DLight, PLight, ALight, PLightHelper, DLightHelper} from './assets/lights'
 
-console.log('this is working')
-
-const model = document.querySelector('[gltf-model]');
-   model.addEventListener('click', () => {
-       const position = model.getAttribute('position');
-       localStorage.setItem('modelPosition', JSON.stringify(position));
-   });
-
-const savedPosition = JSON.parse(localStorage.getItem('modelPosition'));
-   if (savedPosition) {
-       model.setAttribute('posi tion', savedPosition);
-   }
-
-
 try {
 // Add 3D object
 scene.add(camera, DLight, PLight, ALight, PLightHelper, DLightHelper, Torus, cube1, cubeAxis, controls(camera, renderer));
