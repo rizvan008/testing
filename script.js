17544d0b0d2fc79f1e8d49fa97277f4ee66a01be
rizvan008 <108735760+rizvan008@users.noreply.github.com>
2025-02-28 20:43:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("test js worked")
+
+
+console.log('this is working')
+
+const model = document.querySelector('[gltf-model]');
+   model.addEventListener('click', () => {
+       const position = model.getAttribute('position');
+       localStorage.setItem('modelPosition', JSON.stringify(position));
+   });
+
+const savedPosition = JSON.parse(localStorage.getItem('modelPosition'));
+   if (savedPosition) {
+       model.setAttribute('posi tion', savedPosition);
+   }
+
